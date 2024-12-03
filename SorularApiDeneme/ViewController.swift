4a830cf119ca41ea1bf7deb81a1cfecdba779e71
alikoc107 <alikoc107@hotmail.com>
2024-12-03 23:55:06 +0300

    Initial Commit

---
+++ SorularApiDeneme/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SorularApiDeneme
+//
+//  Created by Ali Koç on 3.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
